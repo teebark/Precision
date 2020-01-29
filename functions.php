51ffcc629cc8357a0c4eb546213214804fb1e477
Terry Barker <terry@teebark.com>
2020-01-29 14:04:07 -0500

    Baseline

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+function print_menu_shortcode($atts, $content = null) {
+    extract(shortcode_atts(array( 'name' => null, ), $atts));
+    return wp_nav_menu( array( 'menu' => $name, 'echo' => false ) );
+}
+add_shortcode('menu', 'print_menu_shortcode');
+?>
